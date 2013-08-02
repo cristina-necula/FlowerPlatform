3559a8f81458402c758b4c6c15752a3778bca6ec
CristinaNecula <cristina.necula20@gmail.com>
2013-08-02 13:26:13 +0300

    fg

---
+++ build/classes-generated/org/myorg/flowerplatform/Bundle.java
@@ -0,0 +1,13 @@
+package org.myorg.flowerplatform;
+/** Localizable strings for {@link org.myorg.flowerplatform}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Flower Platform</i>
+     * @see OpenBrowser
+     */
+    static String CTL_OpenBrowser() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_OpenBrowser");
+    }
+    private void Bundle() {}
+}
